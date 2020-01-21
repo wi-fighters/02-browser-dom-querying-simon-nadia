5e190ed5d9092cf29bc362f188c8912a464ecb89
Simon <simonsch.tz@gmail.com>
2020-01-21 10:42:51 +0100

    Finish bg-color generator

--- index.js
+++ index.js
@@ -12,22 +12,22 @@ const category = document.querySelectorAll(".category").forEach(
 )
 
 const colorGenerator = (() => {
-    // loop 3x randomRGB
-    // push to array
-    //push opacity
-    // color = ([randomRGBx3, randomOpacity])
-    const randomOpacity = Math.random().toFixed(3);
-    const rgba = [];
-    for (i = 0; i < 3; i++) {
-        const randomRGB = Math.ceil(Math.random() * 255)
-        rgba.push(randomRGB)
-    }
-    rgba.push(randomOpacity);
-    const foodCategory = document.querySelectorAll(".food-category").forEach(
+    //Traversing list of .foot-category
+    document.querySelectorAll(".food-category").forEach(
         function (foodCategory) {
+            //create random opacity-number
+            const randomOpacity = Math.random().toFixed(3);
+            const rgba = [];
+
+            for (i = 0; i < 3; i++) {
+                const randomRGB = Math.ceil(Math.random() * 255)
+                rgba.push(randomRGB)
+            }
+
+            rgba.push(randomOpacity);
+            //convert rgba[] to a string to make it usable for css-style syntax
             foodCategory.style.backgroundColor = `rgba( ${rgba.toString()})`;
         }
-    )
-    console.log("rgba: " + rgba.toString())
+    );
 
 })();
