19efd4b023cc2092b22df3c05746491ff3872feb
Simon <simonsch.tz@gmail.com>
2020-01-21 10:54:51 +0100

    Give bg-color generator an interval

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const grab = document.querySelector
-
 const body = document.querySelector("body").style.fontFamily = "Arial";
 
 const h1 = document.querySelector("h1").style.textAlign = "center";
@@ -11,23 +9,25 @@ const category = document.querySelectorAll(".category").forEach(
     }
 )
 
-const colorGenerator = (() => {
-    //Traversing list of .foot-category
-    document.querySelectorAll(".food-category").forEach(
-        function (foodCategory) {
-            //create random opacity-number
-            const randomOpacity = Math.random().toFixed(3);
-            const rgba = [];
+const colorInterval = setInterval(function () {
+    const colorGenerator = (() => {
+        //Traversing list of .foot-category
+        document.querySelectorAll(".food-category").forEach(
+            function (foodCategory) {
+                //create random opacity-number
+                const randomOpacity = Math.random().toFixed(3);
+                const rgba = [];
 
-            for (i = 0; i < 3; i++) {
-                const randomRGB = Math.ceil(Math.random() * 255)
-                rgba.push(randomRGB)
+                for (i = 0; i < 3; i++) {
+                    const randomRGB = Math.ceil(Math.random() * 255)
+                    rgba.push(randomRGB)
+                }
+
+                rgba.push(randomOpacity);
+                //convert rgba[] to a string to make it usable for css-style syntax
+                foodCategory.style.backgroundColor = `rgba( ${rgba.toString()})`;
             }
+        );
+    })();
+}, 50);
 
-            rgba.push(randomOpacity);
-            //convert rgba[] to a string to make it usable for css-style syntax
-            foodCategory.style.backgroundColor = `rgba( ${rgba.toString()})`;
-        }
-    );
-
-})();
