85ed1f8bd15b2788c8d2ff0614d284e074fd0f04
Simon <simonsch.tz@gmail.com>
2020-01-21 10:12:05 +0100

    Use querySelectorAll for the first time

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const body = document.querySelector("body").style;
-body.fontFamily = "Arial";
+const grab = document.querySelector
+
+const body = document.querySelector("body").style.fontFamily = "Arial";
+
+const h1 = document.querySelector("h1").style.textAlign = "center";
+
+const category = document.querySelectorAll(".category").forEach(
+    function (category) {
+        category.style.fontFamily = "Cambria";
+        category.style.color = "red";
+    }
+)
+
